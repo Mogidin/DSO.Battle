eb59d8ab669e1adc2fd0b32d933d9df01f48b9bb
Vladimir Kaznacheev <mogidin@gmail.com>
2011-06-06 15:19:36 +0400

    обновление алгоритма формирования результата

--- python/DSO.py
+++ python/DSO.py
@@ -1,8 +1,12 @@
 # -*- coding: UTF-8 -*-
 
 ImgDir = "img/"
+
+MarkTxt = "Mark"
+Mark = ImgDir+"mark.png"
+Back = ImgDir+"back.png"
+
 Unit = {"Rekruten":ImgDir+"Rekruten.png", \
 	"Soldaten":ImgDir+"Soldaten.png", \
 	"Maat":ImgDir+"Maat.png"}
-MarkTxt = "Mark"
-Mark = ImgDir+"mark.png"
+
--- python/runme.py
+++ python/runme.py
@@ -16,30 +16,33 @@ l = (NumArg - 1) / 3 # количество юнитов
 
 MarkPresent = DSO.MarkTxt in sys.argv 
 
-res = Image.new("RGB", (l*baseX+MarkPresent*markX, baseY))
+res = Image.new("RGB", (l*baseX+MarkPresent*markX, baseY)) # формируемая картинка
 
 MarkPrint = 0
 for i in range (l+MarkPresent): # l=2 > range=[0, 1]
 	if sys.argv[1+i*3] == DSO.MarkTxt:
 		# приклеиваем границу
-		print "есть граница"
+#		print "есть граница"
 		im = Image.open(DSO.Mark)
 		res.paste(im,(i*baseX, 0))
 		MarkPrint = 1
 	else:
-		# формируем картинку
 		Unit     = sys.argv[1+(i-MarkPrint)*3+MarkPrint]
 		EndNum   = sys.argv[2+(i-MarkPrint)*3+MarkPrint]
 		StartNum = sys.argv[3+(i-MarkPrint)*3+MarkPrint]
-		print "%s - %s / %s" % (Unit, EndNum, StartNum)
-		im = Image.open(DSO.Unit[Unit])
-		draw = ImageDraw.Draw(im)
+		# пишем текст на подложке
+#		print "%s - %s / %s" % (Unit, EndNum, StartNum)
+		back = Image.open(DSO.Back) # подложка
+		draw = ImageDraw.Draw(back)
 		draw.setfont(font)
 		str = "%s/%s" % (EndNum, StartNum)
 		(xs, ys) = font.getsize(str)
 		draw.text((30 - xs/2, 70), str)
 		# и приклеиваем
-		res.paste(im,((i-MarkPrint)*baseX+(MarkPrint*markX), 0))
+		im = Image.open(DSO.Unit[Unit])
+
+		res.paste(back,((i-MarkPrint)*baseX+(MarkPrint*markX), 0))
+		res.paste(im,((i-MarkPrint)*baseX+(MarkPrint*markX)+2, 2))
 
 res.save("out.png")
 
