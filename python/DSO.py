3a1028be91e1fbff2e4c6864ea1b67e691c82301
Vladimir Kaznacheev <mogidin@gmail.com>
2011-06-06 15:29:18 +0400

    обновлен список юнитов в DSO.py

--- python/DSO.py
+++ python/DSO.py
@@ -6,7 +6,39 @@ MarkTxt = "Mark"
 Mark = ImgDir+"mark.png"
 Back = ImgDir+"back.png"
 
-Unit = {"Rekruten":ImgDir+"Rekruten.png", \
-	"Soldaten":ImgDir+"Soldaten.png", \
-	"Maat":ImgDir+"Maat.png"}
+Unit = { \
+"Armbrustschutze":ImgDir+"Armbrustschütze.png", \
+"Berserker":ImgDir+"Berserker.png", \
+"Bogenschutzen":ImgDir+"Bogenschützen.png", \
+"BrullenderStier":ImgDir+"BrüllenderStier.png", \
+"Deckschrubber":ImgDir+"Deckschrubber.png", \
+"DickeBertha":ImgDir+"DickeBertha.png", \
+"DieWildeWaltraud":ImgDir+"DieWildeWaltraud.png", \
+"EinaugigerBert":ImgDir+"EinäugigerBert.png", \
+"Elitesoldat":ImgDir+"Elitesoldat.png", \
+"Huskarl":ImgDir+"Huskarl.png", \
+"Jomswikinger":ImgDir+"Jomswikinger.png", \
+"Kanonier":ImgDir+"Kanonier.png", \
+"Karl":ImgDir+"Karl.png", \
+"Krahenfuse":ImgDir+"Krähenfüße.png", \
+"Langbogenschutzen":ImgDir+"Langbogenschützen.png", \
+"Maat":ImgDir+"Maat.png", \
+"Messerwerfer":ImgDir+"Messerwerfer.png", \
+"Metallgebiss":ImgDir+"Metallgebiss.png", \
+"Miliz":ImgDir+"Miliz.png", \
+"Plunderer":ImgDir+"Plünderer.png", \
+"Raufbold":ImgDir+"Raufbold.png", \
+"Reiterei":ImgDir+"Reiterei.png", \
+"Rekruten":ImgDir+"Rekruten.png", \
+"Schlager":ImgDir+"Schläger.png", \
+"SirRobin":ImgDir+"SirRobin.png", \
+"Soldaten":ImgDir+"Soldaten.png", \
+"Stinktier":ImgDir+"Stinktier.png", \
+"Thrall":ImgDir+"Thrall.png", \
+"VerruckterSmutje":ImgDir+"VerrückterSmutje.png", \
+"Wachhund":ImgDir+"Wachhund.png", \
+"Waldlaufer":ImgDir+"Waldläufer.png", \
+"Walkure":ImgDir+"Walküre.png", \
+"Wolf":ImgDir+"Wolf.png" \
+}
 
