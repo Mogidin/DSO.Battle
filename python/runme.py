695f29b912c4449fdecabf0dbd1dcf0f98950c6b
Vladimir Kaznacheev <mogidin@gmail.com>
2011-06-19 20:05:45 +0400

    переход на использование sqlite базы

--- python/runme.py
+++ python/runme.py
@@ -2,8 +2,10 @@
 
 import sys
 import os
+import sqlite3
 import Image, ImageDraw, ImageFont
-import DSO
+import DSO, DSOsql
+
 
 (baseX, baseY) = (60, 90) # размер картинки юнита
 (markX, markY) = (12, 90) # размер картинки границы
@@ -23,7 +25,7 @@ for i in range (l+MarkPresent): # l=2 > range=[0, 1]
 	if sys.argv[1+i*3] == DSO.MarkTxt:
 		# приклеиваем границу
 #		print "есть граница"
-		im = Image.open(DSO.Mark)
+		im = DSOsql.GetImage("mark")
 		res.paste(im,(i*baseX, 0))
 		MarkPrint = 1
 	else:
@@ -32,14 +34,14 @@ for i in range (l+MarkPresent): # l=2 > range=[0, 1]
 		StartNum = sys.argv[3+(i-MarkPrint)*3+MarkPrint]
 		# пишем текст на подложке
 #		print "%s - %s / %s" % (Unit, EndNum, StartNum)
-		back = Image.open(DSO.Back) # подложка
+		back = DSOsql.GetImage("back")
 		draw = ImageDraw.Draw(back)
 		draw.setfont(font)
 		str = "%s/%s" % (EndNum, StartNum)
 		(xs, ys) = font.getsize(str)
 		draw.text((30 - xs/2, 70), str)
 		# и приклеиваем
-		im = Image.open(DSO.Unit[Unit])
+		im = DSOsql.GetImage(Unit)
 
 		res.paste(back,((i-MarkPrint)*baseX+(MarkPrint*markX), 0))
 		res.paste(im,((i-MarkPrint)*baseX+(MarkPrint*markX)+2, 2))
